7ec98f907833ac91f114cbfd0681639bdc6c75d0
kujon <jakub.korzeniowski@gmail.com>
2016-09-21 16:56:53 +0100

    Storybook bootstrap

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'latest']
+                }
+            }
+        ]
+    }
+};
